6cfc1db29417d1448aa93b148ba0e8d740d7a5a7
Carolina Palma <166434363+soycaropdh@users.noreply.github.com>
2024-04-26 16:06:41 -0500

    subiendo Backend de la API registro de ventas
    se realizo un CRUD para llevar un registro de las ventas de una tienda de zapatos, este calcula el valor total de manera automática.

---
+++ apiRegistroDeVentas/apiRegistroDeVentas/src/main/java/com/unicartagena/apiRegistroDeVentas/ApiRegistroDeVentasApplication.java
@@ -0,0 +1,13 @@
+package com.unicartagena.apiRegistroDeVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegistroDeVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegistroDeVentasApplication.class, args);
+	}
+
+}
---
+++ apiRegistroDeVentas/apiRegistroDeVentas/src/test/java/com/unicartagena/apiRegistroDeVentas/ApiRegistroDeVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicartagena.apiRegistroDeVentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegistroDeVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
